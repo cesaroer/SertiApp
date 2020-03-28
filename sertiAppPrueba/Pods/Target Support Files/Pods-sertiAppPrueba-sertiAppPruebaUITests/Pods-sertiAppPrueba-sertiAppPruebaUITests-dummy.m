772af88864c1b1ff663c76d211098469a4ded2ad
CesarVargas <cesarvarcard@gmail.com>
2020-03-27 21:57:48 -0600

    Se desinstala Alamofire por problema de compatibilidad

--- sertiAppPrueba/Pods/Target Support Files/Pods-sertiAppPrueba-sertiAppPruebaUITests/Pods-sertiAppPrueba-sertiAppPruebaUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sertiAppPrueba_sertiAppPruebaUITests : NSObject
-@end
-@implementation PodsDummy_Pods_sertiAppPrueba_sertiAppPruebaUITests
-@end
--- sertiAppPrueba/Pods/Target Support Files/Pods-sertiAppPruebaTests/Pods-sertiAppPruebaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sertiAppPruebaTests : NSObject
-@end
-@implementation PodsDummy_Pods_sertiAppPruebaTests
-@end
